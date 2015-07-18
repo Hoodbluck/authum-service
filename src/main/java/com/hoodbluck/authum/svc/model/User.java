c50596dd66b4a091cc9a778a892c3b6cd166a764
Adrian Pena <rastadrian@gmail.com>
2015-07-18 14:18:43 -0400

    Added auto-generated id for user, and annotated the model.

--- src/main/java/com/hoodbluck/authum/svc/model/User.java
+++ src/main/java/com/hoodbluck/authum/svc/model/User.java
@@ -1,5 +1,12 @@
 package com.hoodbluck.authum.svc.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * User's model.
  * Created on 7/18/15.
@@ -7,7 +14,12 @@ package com.hoodbluck.authum.svc.model;
  * @see <a href="https://github.com/Hoodbluck/doc/wiki/Models#user">Authum's Model Guide</a>
  * @author Adrian Pena
  */
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @JsonIgnore
+    private int id;
     private String userId;
     private String firstName;
     private String lastName;
@@ -16,6 +28,23 @@ public class User {
     private String deviceToken;
 
     public User() {
+    }
+
+    public User(String userId, String firstName, String lastName, String email, String password, String deviceToken) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.deviceToken = deviceToken;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getUserId() {
