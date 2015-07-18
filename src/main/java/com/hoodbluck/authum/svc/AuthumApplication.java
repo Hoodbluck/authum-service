2da0dcb9769cd4b03b9e5cb76e8a4ad79d433ee2
Adrian Pena <rastadrian@gmail.com>
2015-07-18 13:51:23 -0400

    Project initial commit

---
+++ src/main/java/com/hoodbluck/authum/svc/AuthumApplication.java
@@ -0,0 +1,12 @@
+package com.hoodbluck.authum.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthumApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hoodbluck/authum/svc/AuthumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hoodbluck.authum.svc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AuthumApplication.class)
+public class AuthumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
