d73c77b298480311f842575cee9828785f4d0eee
Adrian Pena <rastadrian@gmail.com>
2015-07-19 05:34:23 -0400

    Added access control allow origin to requestAuthorizationByUserId

--- src/main/java/com/hoodbluck/authum/svc/controller/ClientController.java
+++ src/main/java/com/hoodbluck/authum/svc/controller/ClientController.java
@@ -6,9 +6,15 @@ import com.hoodbluck.authum.svc.model.AuthumResponse;
 import com.hoodbluck.authum.svc.model.Client;
 import com.hoodbluck.authum.svc.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created on 7/19/15.
@@ -39,8 +45,13 @@ public class ClientController {
             value = "/{clientId}/user/{userId}/auth",
             method = RequestMethod.GET
     )
-    public AuthumResponse requestAuthorizationByUserId(@PathVariable("clientId") String clientId, @PathVariable("userId") int userId) {
-        return mClientManager.requestAuthorization(clientId, userId);
+    public ResponseEntity<AuthumResponse> requestAuthorizationByUserId(@PathVariable("clientId") String clientId, @PathVariable("userId") int userId) {
+        AuthumResponse body = mClientManager.requestAuthorization(clientId, userId);
+        List<String> value = new ArrayList<String>();
+        value.add("*");
+        LinkedMultiValueMap<String,String> headers = new LinkedMultiValueMap<String, String>();
+        headers.put("Access-Control-Allow-Origin", value);
+        return new ResponseEntity<AuthumResponse>(body, headers, HttpStatus.OK);
     }
 
     @RequestMapping(
