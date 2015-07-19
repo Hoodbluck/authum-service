750a9f36d0f12f4d52627d35a2726b152596563f
Adrian Pena <rastadrian@gmail.com>
2015-07-19 04:52:45 -0400

    Added conditionals to avoid duplication of listeners on AuthorizationRequestHandler

--- src/main/java/com/hoodbluck/authum/svc/authentication/AuthorizationRequestHandler.java
+++ src/main/java/com/hoodbluck/authum/svc/authentication/AuthorizationRequestHandler.java
@@ -4,6 +4,7 @@ import com.hoodbluck.authum.svc.model.Client;
 import com.hoodbluck.authum.svc.model.User;
 import com.hoodbluck.authum.svc.util.StringHelper;
 
+import java.util.Iterator;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
@@ -16,14 +17,26 @@ public class AuthorizationRequestHandler {
             = new CopyOnWriteArrayList<AuthorizationRequestListener>();
 
     public void registerListener(AuthorizationRequestListener listener) {
+        for(AuthorizationRequestListener registeredListeners : mRequestListeners) {
+            if(StringHelper.equals(listener.getUser().getEmail(), registeredListeners.getUser().getEmail())
+                    && StringHelper.equals(listener.getClient().getClientId(), registeredListeners.getClient().getClientId())) {
+                System.out.println("A listener for " + listener.getUser().getUserId() + " @ " + listener.getClient().getClientId() + " is already registered, ignoring...");
+                return;
+            }
+        }
+        System.out.println("A listener for " + listener.getUser().getUserId() + " @ " + listener.getClient().getClientId() + " got registered.");
         mRequestListeners.add(listener);
     }
 
     public void notifyAuthenticationResponse(User user, Client client, boolean authenticated) {
-        for (AuthorizationRequestListener listener : mRequestListeners) {
+        Iterator<AuthorizationRequestListener> iterator = mRequestListeners.iterator();
+        while(iterator.hasNext()) {
+            AuthorizationRequestListener listener = iterator.next();
             if (StringHelper.equals(user.getEmail(), listener.getUser().getEmail())
                     && StringHelper.equals(client.getClientId(), listener.getClient().getClientId())) {
                 listener.handleResponse(authenticated);
+                System.out.println("Handled authentication response for " + user.getUserId() + " @ " + client.getClientId());
+                iterator.remove();
             }
         }
     }
